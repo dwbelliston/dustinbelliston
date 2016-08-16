01319586cfd3692e13bb40a63db60ace12573c59
Dustin Belliston <dustinbelliston@gmail.com>
2016-08-15 20:48:18 -0600

    Build image src in componentjs

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -30,7 +30,7 @@ module.exports = {
       },
       {
         test: /\.(png|jpe?g|gif|svg)$/,
-        loader: 'file?name=assets/[name].[hash.[ext]'
+        loader: 'file?name=assets/[name].[hash].[ext]'
       },
       {
         test: /\.css$/,
