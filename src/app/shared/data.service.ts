c6a70703048313503f5cabf2b9f580061e385941
Dustin Belliston <dustinbelliston@gmail.com>
2016-08-18 02:01:05 -0600

    Added content and view more icon

--- src/app/shared/data.service.ts
+++ src/app/shared/data.service.ts
@@ -2,66 +2,68 @@ import { Injectable } from '@angular/core';
 
 export const CLIENTS = [
   {name: `clientsuccess`, img: `work/logo_clientsuccess`,
-    color: `#1ebaff`, url: ``, viewMore: `false`,
+    color: `#1ebaff`, url: `https://www.clientsuccess.com/`, viewMore: true,
     content: `Interactive grid that displays a company’s clients and information about them. Custom filtering and action items.`
   },
   {name: `simplecitizen`, img: `work/logo_simplecitizen`,
-    color: `#246d5a`, url: ``, viewMore: `false`,
-    content: `Takes a user through application process for immigration forms. Think, turbo tax. Generates compelted PDF.`
+    color: `#246d5a`, url: `https://simplecitizen.com/`, viewMore: false,
+    content: `Takes a user through application process for immigration forms. Think, turbo tax. Generates completed PDF.`
   },
   {name: `sendoutcards`, img: `work/logo_soc`,
-    color: `#00BBC6`, url: ``, viewMore: `false`,
-    content: `Interactive grid that displays a company’s clients and information about them. Custom filtering and action items.`
+    color: `#00BBC6`, url: `https://www.sendoutcards.com/catalog/`, viewMore: false,
+    content: `Worked on a grid that showed the cards, pinterest style, and the user could select it, view it and then
+    continue on to customize it. Custom sidenav popout for fitlering. Pinterest styled search input tags.`
   },
   {name: `sol`, img: `work/logo_sol`,
-    color: `#F7AE35`, url: ``, viewMore: `false`,
-    content: `Interactive grid that displays a company’s clients and information about them. Custom filtering and action items.`
+    color: `#F7AE35`, url: ``, viewMore: false,
+    content: `Application for healthcare insurance. Angular frontend to collect user information. Django templating to generate
+    the PDF.`
   },
   {name: `startstudio`, img: `work/logo_startstudio`,
-    color: `#1D7BCB`, url: ``, viewMore: `false`,
-    content: `Interactive grid that displays a company’s clients and information about them. Custom filtering and action items.`
+    color: `#1D7BCB`, url: ``, viewMore: false,
+    content: `Marketing site to promote a startup incubator. Simple, yet elegant.`
   },
   {name: `everyname`, img: `work/logo_everyname`,
-    color: `#145284`, url: ``, viewMore: `false`,
-    content: `Interactive grid that displays a company’s clients and information about them. Custom filtering and action items.`
+    color: `#145284`, url: ``, viewMore: false,
+    content: `Admin site to manage client information and create orders. Lots of models. Lots of fields. Many forms.`
   },
 ];
 
 export const TECH = [
   {name: `angular`, img: `tech/angular`,
-    color: `#E23137`, url: ``, viewMore: `false`,
+    color: `#E23137`, url: ``, viewMore: false,
     content: `Interactive grid that displays a company’s clients and information about them. Custom filtering and action items.`
   },
   {name: `typescript`, img: `tech/typescript`,
-    color: `#0078CF`, url: ``, viewMore: `false`,
+    color: `#0078CF`, url: ``, viewMore: false,
     content: `Takes a user through application process for immigration forms. Think, turbo tax. Generates compelted PDF.`
   },
   {name: `css`, img: `tech/css`,
-    color: `#25A8DE`, url: ``, viewMore: `false`,
+    color: `#25A8DE`, url: ``, viewMore: false,
     content: `Interactive grid that displays a company’s clients and information about them. Custom filtering and action items.`
   },
   {name: `python`, img: `tech/python`,
-    color: `#FFD947`, url: ``, viewMore: `false`,
+    color: `#FFD947`, url: ``, viewMore: false,
     content: `Interactive grid that displays a company’s clients and information about them. Custom filtering and action items.`
   },
   {name: `django`, img: `tech/django`,
-    color: `#06311E`, url: ``, viewMore: `false`,
+    color: `#06311E`, url: ``, viewMore: false,
     content: `Interactive grid that displays a company’s clients and information about them. Custom filtering and action items.`
   },
   {name: `aws`, img: `tech/aws`,
-    color: `#f99900`, url: ``, viewMore: `false`,
+    color: `#f99900`, url: ``, viewMore: false,
     content: `Interactive grid that displays a company’s clients and information about them. Custom filtering and action items.`
   },
   {name: `jquery`, img: `tech/jquery`,
-    color: `#0466B0`, url: ``, viewMore: `false`,
+    color: `#0466B0`, url: ``, viewMore: false,
     content: `Interactive grid that displays a company’s clients and information about them. Custom filtering and action items.`
   },
   {name: `firebase`, img: `tech/firebase`,
-    color: `#f88300`, url: ``, viewMore: `false`,
+    color: `#f88300`, url: ``, viewMore: false,
     content: `Interactive grid that displays a company’s clients and information about them. Custom filtering and action items.`
   },
   {name: `postgres`, img: `tech/postgres`,
-    color: `#2f6592`, url: ``, viewMore: `false`,
+    color: `#2f6592`, url: ``, viewMore: false,
     content: `Interactive grid that displays a company’s clients and information about them. Custom filtering and action items.`
   },
 ];
@@ -76,7 +78,7 @@ export const EDUCATION = [
     content: `Takes a user through application process for immigration forms. Think, turbo tax. Generates compelted PDF.`
   },
   {name: `back-end development`, img: false,
-    color: `#00BBC6`, url: ``, viewMore: false,
+    color: `#00BBC6`, url: ``, viewMore: true,
     content: `Interactive grid that displays a company’s clients and information about them. Custom filtering and action items.`
   },
   {name: `systems admin`, img: false,
