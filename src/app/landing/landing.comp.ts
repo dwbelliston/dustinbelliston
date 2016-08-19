9686a1722171841ce98cdd87b505a855b440bd8c
Dustin Belliston <dustinbelliston@gmail.com>
2016-08-18 20:50:10 -0600

    Hero text change title

--- src/app/landing/landing.comp.ts
+++ src/app/landing/landing.comp.ts
@@ -1,27 +1,34 @@
 import { Component, OnInit, ViewEncapsulation, Renderer } from '@angular/core';
 
+import { RandomColorService } from '../shared/random-color.service'
 
 @Component({
     selector: 'landing',
     template: require('./landing.comp.html'),
-    styles: [require('./landing.comp.scss'), require('../../styles/main.scss')]
+    styles: [require('./landing.comp.scss'), require('../../styles/main.scss')],
+    providers: [RandomColorService]
 })
 export class LandingComponent implements OnInit {
+    private heroElm: any;
 
-    constructor(private renderer : Renderer){
+    constructor(private renderer : Renderer, private randomColor: RandomColorService){
     }
 
     ngOnInit () {
-        // console.log('#--Init--#');
     }
 
     ngAfterContentInit () {
     }
 
     ngAfterViewInit () {
+        this.heroElm = document.getElementById('mainHero');
         this.renderer.listenGlobal( 'window' , 'scroll' , (e : any) => {
             this.onScroll(e);
         });
+
+        setInterval(()=>{
+            this.heroElm.style.color = this.randomColor.getRandom();
+        }, 1000);
     }
 
     onScroll(e: any) {
